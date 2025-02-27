eb42959e98bf038faa26176a9edb87f463ce52b6
Ondřej Budai <ondrej@budai.cz>
2025-02-27 14:44:23 +0100

    Add a smoke test & run on schedule

--- README.md
+++ README.md
@@ -2,4 +2,17 @@
 
 This repository builds and hosts experimental riscv64 images of Fedora Minimal.
 
-[Download the latest image (2025-02-27)](https://github.com/ondrejbudai/fedora-minimal-riscv64/actions/runs/13562656131/artifacts/2661950639)
+[Download the latest Fedora 41 image (2025-02-27)](https://github.com/ondrejbudai/fedora-minimal-riscv64/actions/runs/13562656131/artifacts/2661950639)
+
+This build was smoke-tested using QEMU. To launch it on Fedora, unzip it, and run:
+
+```bash
+qemu-system-riscv64 \
+        -smp cpus=4,maxcpus=4 \
+        -m 2G \
+        -machine virt,acpi=off \
+        -device virtio-net-pci,netdev=n0,mac="FE:0B:6E:23:3D:9A" \
+        -netdev user,id=n0,net=10.0.2.0/24,hostfwd=tcp::2225-:22 \
+        -drive if=pflash,format=raw,unit=0,file=/usr/share/edk2/riscv/RISCV_VIRT_CODE.fd,readonly=on \
+        -drive file=./disk.raw
+```
---
+++ test.py
@@ -0,0 +1,67 @@
+import pexpect
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: python3 test.py <image> <firmware>")
+    sys.exit(1)
+
+image = sys.argv[1]
+firmware = sys.argv[2]
+
+f = open("log", "w")
+
+child = pexpect.spawn(
+    """qemu-system-riscv64 \
+        -smp cpus=5,maxcpus=5 \
+        -m 2G \
+        -machine virt,acpi=off \
+        -device virtio-net-pci,netdev=n0,mac="FE:0B:6E:23:3D:9A" \
+        -netdev user,id=n0,net=10.0.2.0/24,hostfwd=tcp::2225-:22 \
+        -drive if=pflash,format=raw,unit=0,file=./RISCV_VIRT_CODE.fd,readonly=on \
+        -drive file=./disk.raw -snapshot -nographic"""
+)
+
+child.logfile = f.buffer
+
+child.expect("Please make a selection from the above", timeout=120)
+print("configuring root")
+child.sendline("4")
+
+child.expect("Password:")
+print("password 1st")
+child.sendline("rootroot")
+
+child.expect("Password")
+print("password 2nd")
+child.sendline("rootroot")
+
+child.expect("Please respond 'yes' or 'no':")
+print("confirming")
+child.sendline("yes")
+
+
+child.expect("Please make a selection from the above")
+print("continuing")
+child.sendline("c")
+
+
+child.expect("localhost login:")
+print("logging in")
+child.sendline("root")
+
+child.expect("Password:")
+print("pasword 1st")
+child.sendline("rootroot")
+
+child.expect(r"~\]#")
+print("running command")
+child.sendline("systemctl is-system-running")
+
+child.expect(r"~\]#")
+assert "running" in child.before.decode("utf-8")
+
+print("shutting down")
+child.sendline("systemctl poweroff")
+child.expect(pexpect.EOF)
+
+child.wait()
